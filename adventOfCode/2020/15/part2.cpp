e1f9a841a5031bf75741f3846e03cc637a1a4a99
Matej Sestak <sestamat@fel.cvut.cz>
2020-12-15 08:48:48 +0100

    AoC day 15

---
+++ adventOfCode/2020/15/part2.cpp
@@ -0,0 +1,26 @@
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unordered_map<int, int> mem;
+    char p;
+    int spoken = 0, before = -1;
+    for (int i = 1, num; cin >> num; ++i){
+        mem[num] = i;
+        spoken = num;
+        cin >> p;
+    }
+    auto turn = mem.size();
+    while (turn < 30000000){
+        ++turn;
+//        cout << "t: " << turn ;
+        spoken = before == -1 ? 0 : turn - 1 - before;
+        before = mem.find(spoken) != mem.end() ? mem[spoken] : -1;
+        mem[spoken] = turn;
+//        cout << " s " << spoken << " b " << before << endl;;
+    }
+    cout << spoken << endl;
+
+}
